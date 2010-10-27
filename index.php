1a169f3f38915b62ae49ca14011dea463d721bdc
swgroup <swgroup@ubuntu-laptop-server.(none)>
2010-10-27 17:52:18 -0600

    Initial Commit

--- index.php
+++ index.php
@@ -1,8 +1,39 @@
-<?php
-?>	
-<div class="content">
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+
+<html>
+	<head>
+		<link href="css/stylesheet.css" rel="stylesheet" type="text/css" />
+
+		<title>CrapOut!<title>
+	</head>
+	<body>
+	<div class="content">
 		<h1>Crapout Landing Page</h1>
-	<p> This is the landing page for crapout.quistaliscio.us, used for staging, prototyping, etc. Have a look around.</p>
+	<p>Welcome to the future home of CrapOut!: a Java-based online Craps game.<br /> To get ready to play, brush up on the Rules as well as the Odds of Craps. </p>
+	<h2>Rules</h2>
+	<p>What is Craps? Craps is a game of round that consists of a player rolling the 2 dice, shooter.<br /> Bets can be made on the table according to the outcomes of the rolls of the 2 dice.<br /><br />
+
+How to Play: Game starts when the shooter rolls the dice for the first time, come out roll. If the roll<br /> results in a 7 or 11 that is an Automatic win and results in a win for Pass Line bets. The shooter now gets<br /> to keep the dice and is entitled to another &#8220;come out roll&#8221;.  If the roll results in a 2, 3, or 12 that<br /> is Craps and results in a lost for Pass Line bets. The shooter still keeps the dice and is entitled <br />to another &#8220;come out roll&#8221;. If a 4, 5, 6, 8, 9, or 10 is rolled than that number is called the point. A <br />white puck will then be placed on the table to designate what that point is. If a point is rolled the <br />dice will be rolled continuously until the same point is rolled again or a 7 is rolled.<br /><br />
+If the shooter establishes a point, the series continues and the shooter continues to roll the dice. The <br />object of the game now changes and the goal is for the shooter to roll the point number again before rolling a 7.<br /><br />
+- If any number other than the point or a 7 is rolled, nothing happens, series continues and shooter rolls again.<br /><br />
+- If the point is rolled, Pass Line bets win and the series end.<br /><br />
+- If a 7 is rolled, Pass Line bets lose (seven out) and series end. <br /><br />
+	</p>
+	<h2>Odds</h2>
+ 		<p>Pass odds: If a 4, 5, 6, 8, 9, or 10 is thrown on the come-out roll (i.e., if a point is set), most casinos<br /> allow pass line bettors to take odds by placing from one to five times (and at some casinos, up to 100 times)<br /> the pass line bet behind the pass line. This additional bet wins if the point is rolled again before a 7 is rolled<br /> (the point is made) and pays at the true odds of 2-to-1 if 4` or 10 is the point, 3-to-2 if 5 or 9 is the point, and 6-to-5 if 6 or 8 is the point.<br /><br />
+
+Some casinos offer "3-4-5X Odds," where the maximum allowed odds bet depends on the point (three times if the<br /> point is 4 or 10, four times on 5 or 9, and five times on 6 or 8). This rule simplifies the calculation<br /> of winnings: a maximum pass odds bet on a 3-4-5X table will always be paid at six times the pass line bet<br /> regardless of the point.<br /><br />
+
+As odds bets are paid at true odds, in contrast with the pass line which is always even money, playing pass <br />odds on a minimum pass line bet lessens the house advantage. A maximum odds bet on a minimum pass line bet <br />often gives the lowest house edge available in the casino. However, the odds bet cannot be made <br />independently, so the house retains an edge on the pass line bet itself.<br /><br />
+
+Don't pass odds: If a player is playing don't pass instead of pass, they may also lay odds by placing chips <br />behind the don't pass line. If a 7 comes instead of the point coming, the odds pay at true odds of 1-to-2 <br />if 4 or 10 is the point, 2-to-3 if 5 or 9 is the point, 5-to-6 if 6 or 8 is the point.<br /><br />
+	</p>
+	<h3>References</h3>
+	<p>Kanzen, J. (n.d.). Craps Rules. Retrieved October 21, 2010, from Gamblin il dado: http://www.ildado.com/craps_rules.html<br /> <br />
+
+Parkansky, K. (n.d.). Basic Craps Play. Retrieved October 21, 2010, from Going to Vegas: http://www.goingtovegas.com/kpv-crap.htm<br /> <br />
+RTG Casinos. (2009). CRAPS. Retrieved October 21, 2010, from Club USA Casino: http://www.rtgcasinos.org/craps.htm<br />
+	</p>
 	</div>
-<?php
-?>
+	<body>
+
